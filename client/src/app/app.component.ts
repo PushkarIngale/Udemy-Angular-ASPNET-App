ff786af397a6d097e1d04066a19ebf5652566900
PushkarIngale <pushkar.i@imocha.io>
2021-10-29 11:47:25 +0530

    End of Section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+      console.log("🚀 ~ file: app.component.ts ~ line 22 ~ AppComponent ~ this.http.get ~ this.users", this.users)
+    }, error => {
+      console.log(error);
+    })
+  }
+}
